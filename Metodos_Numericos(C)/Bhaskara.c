311dae4bb300f53be606bad164d5effd7c592617
Patrick <lteloladeira@gmail.com>
2024-02-24 15:52:16 -0300

    bhaskara

--- Metodos_Numericos(C)/Bhaskara.c
+++ Metodos_Numericos(C)/Bhaskara.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
-float bhaskara(float a, float b, float c);
+float delta(float coefA, float coefB, float coefC);
 
 int main () {
     int grau;
-    float coefA, coefB, coefC;
-    printf("Digite qual o grau do seu polinomio: ");
-    scanf("%i", &grau);
-    bhaskara(coefA, coefB, coefC);
-
-}
-
-float bhaskara(float a, float b, float c) {
-    float delta; 
+    float a, b, c;
+    float x1, x2; 
     printf("Coeficiente A: ");
     scanf("%f", &a);
     printf("Coeficiente B: ");
     scanf("%f", &b);
     printf("Coeficiente C: ");
     scanf("%f", &c);
-    delta = pow(b, 2) - (4 * a * c);
-    
+    float delta_main = delta(a, b, c);
+    x1 = ((-1)*b + sqrt(delta_main))/(4*a);
+    x2 = ((-1)*b - sqrt(delta_main))/(4*a);
+    printf("Os valores de x sao %2.f e %2.f", x1, x2);
+}
 
-
+float delta(float coefA, float coefB, float coefC) {
+    float delta;
+    delta = pow(coefB, 2) - (4 * coefA * coefC);
+    return delta;
+   
 }
