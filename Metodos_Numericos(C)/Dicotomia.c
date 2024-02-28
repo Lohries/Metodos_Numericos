1b6f1f2d882d0b53694ffa7ffc790d1397a4005f
Patrick <lteloladeira@gmail.com>
2024-02-28 19:50:03 -0300

    point collect

---
+++ Metodos_Numericos(C)/Dicotomia.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+float sine();
+float cossine();
+float tangent();
+float none();
+
+int main () {
+    int grau, model;
+    float pointA_x, pointB_x;
+    float pointA_y, pointB_y;
+    printf("Insert based on the type of function (1-sin  2-cos  3-tg  4-None of thos)");
+    scanf("%i", &model);
+    if (model == 1) {
+
+    }
+    else if (model == 2) {
+
+    }
+    else if (model == 3) {
+
+    }
+    else {
+        none(grau, pointA_x, pointA_y, pointB_x, pointB_y);
+
+        
+    }
+  
+
+}
+
+float none(float grau, float pointA_x, float pointA_y, float pointB_x, float pointB_y) {
+    printf("Insert the biggest expoent in your equation: ");
+    scanf("%i", &grau);
+    printf("Insert the point A, x and y in order");
+    scanf("%f, %f", &pointA_x, &pointA_y);
+    printf("Insert the point B, x and y in order");
+    scanf("%f, %f", &pointB_x, &pointB_y);
+
+}
