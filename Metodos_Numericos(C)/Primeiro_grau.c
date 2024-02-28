01fad07443335654fdf958351c62cc600b2f34ba
Patrick <lteloladeira@gmail.com>
2024-02-28 19:28:45 -0300

    final

--- Metodos_Numericos(C)/Primeiro_grau.c
+++ Metodos_Numericos(C)/Primeiro_grau.c
@@ -2,6 +2,14 @@
 #include <math.h>
 
 int main () {
-
+    float a, b;
+    float x;
+    printf("Insert the 'a' parameter, that multiply the variable:");
+    scanf("%f", &a);
+    printf("Insert the 'b' parameter:");
+    scanf("%f", &b);
+    b = (-1) * b;
+    x = b/a;
+    printf("The answer is %2.f", x);
 
 }
