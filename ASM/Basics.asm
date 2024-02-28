fe083f8f1f59d423b878354f7975fa547922b73b
Patrick <lteloladeira@gmail.com>
2024-02-28 14:31:36 -0300

    hello

--- ASM/Basics.asm
+++ ASM/Basics.asm
@@ -1,1 +1,15 @@
-global _main
+
+section .text ;SHF_EXECINSTR
+global _start
+_start:
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    mov eax, 1
+    int 0x80
+
+sectoin .data ;SHF_WRITE
+    msg db "Hello World", 0xa 
+    len equ $ -msg
