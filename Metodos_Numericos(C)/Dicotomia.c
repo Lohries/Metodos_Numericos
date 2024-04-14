7a3b544cb008925d071e87f1ec208000c4fcb8ce
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-04-14 11:46:40 -0300

    lagrange start and dicotomia end

--- Metodos_Numericos(C)/Dicotomia.c
+++ Metodos_Numericos(C)/Dicotomia.c
@@ -2,31 +2,92 @@
 #include <stdlib.h>
 #include <math.h>
 
-void aloca(float **p, int grau);
+int calculando_k(float precisao_k, float intervaloA_k, float intervaloB_k);
+void aloca(int grau_f, float **p);
+void preenchimento(float **p, int grau_p);
+int dicotomia(float intervaloA_D, float intervaloB_D, float **p, int grau_D, float intervaloM);
 
-
-int main () {
-    int grau;
+int main() {
+    int grau, numero_interar, flag_controle;
+    float precisao, intervaloA, intervaloB, intervalo_main;
     float *ptr = NULL;
-    float erro, intervaloA, intervaloB;
-    printf("Digite o grau do polinomio: ");
-    scanf("%f", &grau);
-    aloca(&ptr, grau);
-    for (int i = 0; i < grau; ptr++) {
-        printf("Digite os ceoficientes do mqior grqu qo menor");
-        scanf("%f", ptr);
-    }
-    printf("Digite o valor do erro: ");
-    scanf("%f", &erro);
-    printf("Digite o intervalo de x: ");
+    printf("\nInsira o grau do polinomio: ");
+    scanf("%i", &grau);
+    printf("\nInsira a precisao: ");
+    scanf("%f", &precisao);
+    printf("\nInsira menor extremo: ");
     scanf("%f", &intervaloA);
+    printf("\nInsira o extremo maior: ");
     scanf("%f", &intervaloB);
+    numero_interar = calculando_k(precisao, intervaloA, intervaloB);
+    intervalo_main = intervaloB - intervaloA;
+    float intervaloM = (intervaloA + intervaloB)/2;
+    printf("\nObtendo a funcao do usuario....\n");
+    aloca(grau, &ptr);
+    preenchimento(&ptr, grau);
 
+    while(numero_interar > 0 && intervalo_main > precisao) {
+        flag_controle = dicotomia(intervaloA, intervaloB, &ptr, grau, intervaloM);
+        if (flag_controle == 1) {
+            intervaloA = intervaloM;
+            intervalo_main = intervaloB - intervaloM;
+        }
+        else {
+            intervaloB = intervaloM;
+            intervalo_main = intervaloA - intervaloM;
+
+        }
+        numero_interar--; 
+    }
+    printf("\nFINALIZADA A DICOTOMIA");
+    printf("\nRESULTADO FINAL %f", intervaloM);
+
+    free(ptr); 
+    return 0;
 }
 
-void aloca (float **p, int grau) {
-    *p = ((float)realloc(*p, grau * sizeof(float)))
-    if (*p == NULL)
-        exit(1);
+int calculando_k(float precisao_k, float intervaloA_k, float intervaloB_k) {
+    double k;
+    k = (log(intervaloB_k - intervaloA_k) - log(precisao_k)) / log(2);
+    return round(k);
+}
 
+void aloca(int grau_f, float **p) {
+    *p = (float*)realloc(*p, (grau_f + 1) * sizeof(float));
+    if (*p == NULL) {
+        printf("\nErro ao alocar memória!\n");
+        exit(1);
+    } else {
+        printf("\nContinuando...\n");
+    }
+}
+
+void preenchimento(float **p, int grau_p) {
+    for (int i = grau_p; i >= 0; i--) {
+        printf("\nDigite o coeficiente para o termo de grau %i: ", i);
+        scanf("%f", &(*p)[i]);
+    }
+}
+
+int dicotomia(float intervaloA_D, float intervaloB_D, float **p, int grau_D, float intervaloM) {
+    float funcaoA = 0;
+    float funcaoB = 0;
+    float funcaoM = 0;
+    
+    for (int i = grau_D; i >= 0; i--) {
+        funcaoA += ((*p)[i] * pow(intervaloA_D, i));
+        funcaoB += ((*p)[i] * pow(intervaloB_D, i)); 
+        funcaoM += ((*p)[i] * pow(intervaloM, i)); 
+    }
+    printf("\nFuncaoA: %f", funcaoA);
+    printf("\nFuncaoB: %f", funcaoB);
+    printf("\nFuncaoB: %f", funcaoB);
+    if (funcaoA*funcaoM > 0 && funcaoB*funcaoM < 0) {
+        return -1;
+    }
+
+    if (funcaoA*funcaoM < 0 && funcaoB*funcaoM > 0) {
+        return 1;
+    }
+    
 }
---
+++ Metodos_Numericos(C)/Lagrange.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main () {
+    int n_pontos;
+    printf("Digite a quantidade de pontos: ");
+    scanf("%i", &n_pontos);
+
+
+}
